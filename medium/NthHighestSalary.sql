bc9f34fa078d4697ea257f202a0a923bdfa3fc8c
Jswig <andersjopo@gmail.com>
2020-03-12 17:59:00 -0700

    Nth highest salary

---
+++ medium/NthHighestSalary.sql
@@ -0,0 +1,19 @@
+/* Anders Poirel
+ * 10-03-2020
+ *
+ * https://leetcode.com/problems/nth-highest-salary
+ * Runtime: 304 ms, faster than 60.75% of MySQL online submissions for Nth Highest Salary.
+ * Memory Usage: 0B, less than 100.00% of MySQL online submissions for Nth Highest Salary. */
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE M INT;
+  SET M = N-1;
+  RETURN (
+      SELECT DISTINCT Salary 
+      FROM Employee
+      ORDER BY Salary DESC
+      LIMIT 1 OFFSET M
+      
+  );
+END
